dde9ea5eb22f8147a6c0bf30c4c8340832089faf
VanHuy <2251120293@ut.edu.vn>
2024-08-13 01:16:00 +0700

    Bo sung cac link

---
+++ js/Policy.js
@@ -0,0 +1,18 @@
+document.addEventListener("scroll", function () {
+    const footer = document.getElementById("footer");
+    const rightContact = document.querySelector(".right-contact");
+  
+    const footerRect = footer.getBoundingClientRect();
+    const rightContactRect = rightContact.getBoundingClientRect();
+  
+    // Kiểm tra xem footer có nằm trong viewport không
+    if (footerRect.top < window.innerHeight) {
+      rightContact.style.position = "absolute";
+      rightContact.style.top = `${window.scrollY + footerRect.top - rightContactRect.height}px`;
+    } else {
+      rightContact.style.position = "fixed";
+      rightContact.style.top = "50%";
+      rightContact.style.transform = "translateY(-50%)";
+    }
+  });
+  
