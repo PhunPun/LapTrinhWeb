a37b0cc40e8b3467170e8660adbed0d9f99ddbe6
VanHuy <2251120293@ut.edu.vn>
2024-08-13 10:38:01 +0700

    HUy

--- js/Contact.js
+++ js/Contact.js
@@ -1,9 +1,17 @@
 var w;
-function support() {
-  w = window.open(
-    "/webfonts/Support.html",
-    "sp",
-    "width=400, height=200, top=50,left=600"
-  );
-  w.focus();
-}
+      function support() {
+        w = window.open(
+          "Support.html",
+          "sp",
+          "width=400, height=400, top=50,left=600"
+        );
+        w.focus();
+      }
+      function supportlg() {
+        w = window.open(
+          "Login.html",
+          "lg",
+          "width=400, height=400, top=50,left=600"
+        );
+        w.focus();
+      }
