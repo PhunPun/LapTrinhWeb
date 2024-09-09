1110104cec18f71946bbb0cca0f500cb67aa8ce8
VanHuy <2251120293@ut.edu.vn>
2024-09-10 01:50:36 +0700

    menu

--- admin/admin.js
+++ admin/admin.js
@@ -1,22 +1,43 @@
-function loadIframe(page,element){
+function loadIframe(page, element) {
     document.getElementById('content_iframe').src = page;
+    const menuItems = document.querySelectorAll('.product-subMenu2 .menu-kind');
+    menuItems.forEach(item => item.classList.remove('product-active-menu'));
+    element.classList.add('product-active-menu');
+    document.querySelector('.manage_product').style.display = "none";
+}
 
-    const menuItems = document.querySelectorAll('.product-subMenu2 .menu-kind');
-    menuItems.forEach(item => {
-        item.classList.remove('product-active-menu');
-    })
+function loadIframe1(page, element) {
+    document.getElementById('content_iframe').src = page;
+    const menuItems = document.querySelectorAll('.product-subMenu3 .menu-kind1');
+    menuItems.forEach(item => item.classList.remove('product-active-menu'));
     element.classList.add('product-active-menu');
-    //ẩn 
-    element.classList.add('product-active-menu');
-    const manage = document.querySelector('.manage_product');
-    manage.style.display = "none";
 }
-function choose(element){
+
+function choose(element) {
     const menuItems = document.querySelectorAll('.product-subMenu2 .menu-kind');
-    menuItems.forEach(item => {
-        item.classList.remove('product-active-menu');
-    })
+    menuItems.forEach(item => item.classList.remove('product-active-menu'));
     element.classList.add('product-active-menu');
-    const manage = document.querySelector('.manage_product');
-    manage.style.display = "block";
+    document.querySelector('.manage_product').style.display = "block";
 }
+let isMenuVisible = true;
+
+function toggleMenu() {
+    const menu = document.querySelector('.product-menu');
+    const mainContent = document.querySelector('.product-main-option-over');
+    const toggleIcon = document.querySelector('.toggle-icon');
+
+    if (isMenuVisible) {
+        // Ẩn menu
+        menu.classList.add('product-menu-hidden');
+        mainContent.style.marginLeft = "-278px"; // Đặt lề trái của phần chính về 0
+    } else {
+        // Hiển thị menu
+        menu.classList.remove('product-menu-hidden');
+        mainContent.style.marginLeft = "5px"; // Đặt lề trái của phần chính để làm cho menu không bị đẩy ra ngoài
+    }
+
+    isMenuVisible = !isMenuVisible; // Đảo trạng thái của menu
+}
+
+
+
